4f1f093198357fe4438a7070e767c3655bb5c142
kristymz <69281576+kristymz@users.noreply.github.com>
2020-08-06 12:13:45 -0400

    Add files via upload

---
+++ 7.2 Storyboard/7.2 Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7.2 Storyboard
+//
+//  Created by kristy awesome pants martinez on 8/6/20.
+//  Copyright © 2020 kristy martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
