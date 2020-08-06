041b03550350e4f581232e740694343eede50170
kristymz <69281576+kristymz@users.noreply.github.com>
2020-08-06 12:16:25 -0400

    Add files via upload

---
+++ Intro-to-Xcode&Storyboard/Intro-to-Xcode&Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro-to-Xcode&Storyboard
+//
+//  Created by kristy awesome pants martinez on 8/6/20.
+//  Copyright © 2020 kristy martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing, testing!")
+    }
+
+
+}
+
