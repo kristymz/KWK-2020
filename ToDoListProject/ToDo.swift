54973fe8f46cf6d37b94892fd952c194f0696c78
kristymz <69281576+kristymz@users.noreply.github.com>
2020-08-10 23:25:14 -0400

    Add files via upload
    Completed iteration 0

---
+++ ToDoListProject/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by kristy awesome pants martinez on 8/10/20.
+//  Copyright © 2020 kristy martinez. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+
+   var description = ""
+   var important = false
+
+}
